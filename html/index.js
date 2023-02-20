9362d89a9bd38b921b368dbf6a1b9cb217acefb7
rafanami <rafanami@gmail.com>
2023-02-20 19:47:01 +1300

    Adding removeTodo

--- html/index.js
+++ html/index.js
@@ -3,6 +3,15 @@ function addTodoList(event) {
 }
 
 /**
+ * Removes a todo
+ * @param {*} event 
+ */
+function removeTodo(event) {
+    const item = event.target.parentElement;
+    item.parentElement.removeChild(item);
+}
+
+/**
  * gets the value from the "todo" input and adds it to the list
  * @param {*} event 
  */
@@ -18,8 +27,9 @@ function addTodo(event) {
 
     // create a new list item
     const item = document.createElement('li');
-    item.innerHTML = `<button>Complete</button> ${todo}`;
-    
+    item.id = todo;
+
+    item.innerHTML = `<button onclick="removeTodo(event)">Complete</button> ${todo}`;
     
     // append the list item to the ordered list
     list.appendChild(item);
